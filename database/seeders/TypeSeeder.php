ec01dd50954f10f6037f0509720def184efe9661
eltonanit <elton.ani@gmail.com>
2024-10-15 20:12:16 +0200

    [UPD] DB-seeder TYPE

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
+
 
 class TypeSeeder extends Seeder
 {
@@ -12,8 +14,16 @@ class TypeSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
-    }
+   public function run()
+   {
+    $types = ['Frontend', 'Backend', 'FullStack', 'Database', 'Design'];
+    foreach($types as $type){
+    $new_type = new Type();
+    
+    $new_type->name = $type;
+    $new_type->slug = Type::generateSlug($type);
+    
+    $new_type->save(); 
+  }
+ }
 }
