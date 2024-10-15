0b0b85c346dea3198ccee96a95c867efee42d152
eltonanit <elton.ani@gmail.com>
2024-10-15 19:02:13 +0200

    [UPD] php make migration column projects

--- database/migrations/2024_10_15_170109_add_type_id_column_to_table_projects.php
+++ database/migrations/2024_10_15_170109_add_type_id_column_to_table_projects.php
@@ -13,11 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('types', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 100);
-            $table->string('slug', 150);
-            $table->timestamps();
+        Schema::table('projects', function (Blueprint $table) {
+            //
         });
     }
 
@@ -28,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('types');
+        Schema::table('table_projects', function (Blueprint $table) {
+            //
+        });
     }
 };
